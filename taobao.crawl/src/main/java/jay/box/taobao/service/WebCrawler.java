f1faf613ad39c3b701a2254fbaae2fefbc70a322
lakemove <lakemove@8d3e6b1c-1407-d586-758c-326c60ca0b66>
2010-09-15 15:34:30 +0000

    extract interface WebCrawler and 2 implementations : httpclient crawler and simple crawler move resource file (.xml) from java folder to resources folder where it deserved to be.

--- taobao.crawl/src/main/java/jay/box/taobao/service/WebCrawler.java
+++ taobao.crawl/src/main/java/jay/box/taobao/service/WebCrawler.java
@@ -1,36 +1,16 @@
 package jay.box.taobao.service;
 
-import java.io.IOException;
+import java.io.InputStream;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class WebCrawler {
-
-	@Autowired
-	private HttpClient httpClient;
+public interface WebCrawler {
 	
 	/**
 	 * request for content of the url
 	 * @param url target address to request
 	 * @param responseEncoding in which encoding to read out he response content
-	 * @see org.apache.commons.httpclient.HttpMethodBase#getResponseBodyAsString()
 	 * @return response content
 	 */
-	public String wget(String url){
-		GetMethod gm = new GetMethod(url);
-		try {
-			httpClient.executeMethod(gm);
-			String response = gm.getResponseBodyAsString();
-			return response;
-		} catch (HttpException e) {
-			throw new RuntimeException(e);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+	String wget(String url);
+	
+	InputStream wgetStream(String url);
 }
