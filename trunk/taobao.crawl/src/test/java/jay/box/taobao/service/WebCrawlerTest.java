bbf7b94c137bddefe77eb8212ddcc174f6cbe3be
lakemove <lakemove@8d3e6b1c-1407-d586-758c-326c60ca0b66>
2010-10-07 12:21:39 +0000

    little works 1.removed userId attribute in Buyer (which cause JPA error) 2.make test runnable against SearchedItemIterator 3.add comments on key method/attribute in AbstractPagedResultIterator

--- trunk/taobao.crawl/src/test/java/jay/box/taobao/service/WebCrawlerTest.java
+++ trunk/taobao.crawl/src/test/java/jay/box/taobao/service/WebCrawlerTest.java
@@ -1,6 +1,11 @@
 package jay.box.taobao.service;
 
+import java.util.ArrayList;
+
 import javax.annotation.Resource;
+
+import jay.box.taobao.domain.Item;
+import jay.box.taobao.service.impl.WebIteratorLocator;
 
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit38.AbstractJUnit38SpringContextTests;
@@ -22,5 +27,22 @@ public class WebCrawlerTest extends AbstractJUnit38SpringContextTests {
 	public void testCrawlPage() throws Exception {
 		String resp = simpleWebCrawler.wget("http://www.taobao.com");
 		System.out.println(resp);
+		
+		String html = simpleWebCrawler.wget("http://search.taobao.com/search?q=%CD%E0&commend=all&sort=sale-desc&style=list&isnew=2&source=search1&s=40#J_FilterTabBar");
+		System.out.println(html);
+	}
+	
+	@Resource
+	private WebIteratorLocator locator;
+	
+	public void testWebIteratorLocator() throws Exception {
+		String url = "http://search.taobao.com/search?q=%CD%E0&commend=all&sort=sale-desc&isnew=2&source=search1&style=list&s=0";
+		WebIterator<Item> itemIterator = locator.createInstance(url, null);
+		ArrayList<Item> items = new ArrayList<Item>();
+		while(itemIterator.hasNext()){
+			Item item = itemIterator.next();
+			items.add(item);
+			System.out.println(item);
+		}
 	}
 }
